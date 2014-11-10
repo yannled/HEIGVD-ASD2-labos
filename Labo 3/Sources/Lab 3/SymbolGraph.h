a5352f3a933b82a0d780234268a7027886ef2f2f
ValentinMinder <valentin.minder@epfl.ch>
2014-11-10 16:21:18 +0100

    lab 2 files

---
+++ Labo 3/Sources/Lab 3/SymbolGraph.h
@@ -0,0 +1,115 @@
+/* 
+ * File:   SymbolGraph.h
+ * Author: Olivier Cuisenaire
+ *
+ * Created on 26. septembre 2014, 15:08
+ */
+
+#ifndef SYMBOLGRAPH_CORR_H
+#define	SYMBOLGRAPH_CORR_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#include "Util.h"
+
+namespace ASD2
+{
+    template<typename GraphType>
+    class SymbolGraph
+    {
+        typedef GraphType Graph; 
+    private:
+        std::map<std::string,int> symbolTable;
+        std::vector<std::string> keys;
+        Graph* g;
+        
+    public:
+        
+        ~SymbolGraph()
+        {
+            delete g; 
+        }            
+        
+        //creation du SymbolGraph a partir du fichier movies.txt
+        SymbolGraph(const std::string& filename) {         
+            //lecture du fichier, ligne par ligne puis element par element (separe par des /)
+            std::string line;
+            int cnt=0; 
+
+            std::ifstream s(filename);
+            while (std::getline(s, line))
+            {
+                auto names = ASD2::split(line,'/');
+                for( auto name : names ) 
+                    if(!contains(name))
+                        symbolTable[name] = cnt++;
+                
+                std::cout << std::endl;
+            }
+            s.close();
+            
+            keys.resize(cnt);
+            for(const auto& pair : symbolTable)
+                keys[pair.second] = pair.first; 
+            
+            g = new Graph(cnt);
+            
+            s.open(filename);
+            while (std::getline(s, line))
+            {
+                auto names = ASD2::split(line,'/');
+               
+                int v = symbolTable[names[0]];
+                for( size_t i = 1; i < names.size(); ++i ) {
+                    int w = symbolTable[names[i]];
+                    g->addEdge(v,w);
+                }
+            }
+            s.close();
+            
+        }
+        
+        //verifie la presence d'un symbole
+        bool contains(const std::string& name) const {
+            return symbolTable.find(name) != symbolTable.end();
+        }
+        
+        //index du sommet correspondant au symbole
+        int index(const std::string& name) const {
+            return symbolTable.at(name);
+        }
+        
+        //symbole correspondant au sommet
+        std::string name(int idx) const {
+            return keys[idx];
+        }
+
+        std::vector<std::string> adjacent(const std::string & name) const
+        {
+            if(!contains(name)) 
+                return std::vector<std::string>(0);
+            
+            auto& adjIdx = g->adjacent( index(name));
+            int N = adjIdx.size();
+            std::vector<std::string> adjStr(N);
+            
+            std::transform(adjIdx.cbegin(), adjIdx.cend(), adjStr.begin(), [&](int i){ return this->name(i); } );
+            
+            return adjStr; 
+        }
+        
+        const Graph& G() {
+            return *g; 
+        }
+        
+    };
+    
+}
+
+#endif	/* SYMBOLGRAPH_CORR_H */
---
+++ Labo 3/Sources/Lab 3/Util.cpp
@@ -0,0 +1,22 @@
+
+#include "Util.h"
+#include <sstream>
+
+namespace ASD2
+{
+    std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+        std::stringstream ss(s);
+        std::string item;
+        while (std::getline(ss, item, delim)) {
+            elems.push_back(item);
+        }
+        return elems;
+    }
+
+    std::vector<std::string> split(const std::string &s, char delim) {
+        std::vector<std::string> elems;
+        split(s, delim, elems);
+        return elems;
+    }
+
+}
