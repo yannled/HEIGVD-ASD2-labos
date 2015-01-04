5142965d856a4c138f43bf330e84e23a7801c9bd
ValentinMinder <valentin.minder@epfl.ch>
2015-01-04 12:39:00 +0100

    make it compile - part 2

--- RoadDiGraphWrapper.h
+++ RoadDiGraphWrapper.h
@@ -30,11 +30,12 @@ private:
 
 public:
 
+    typedef ASD2::WeightedDirectedEdge<double> Edge;
     RoadDiGraphWrapper(const RoadNetwork rne) : rn(rne) {
 
     }
 
-    int V() {
+    int V() const {
         return rn.cities.size();
     }
 
@@ -43,7 +44,7 @@ public:
         std::vector<int> e = rn.cities.at(v).roads;
         for (int i = 0; i < e.size(); i++) {
             std::pair<int, int> p = rn.roads.at(e.at(i)).cities;
-            f(ASD2::WeightedEdge<int>(p.first,
+            f(ASD2::WeightedDirectedEdge<double>(p.first,
                     p.second, rn.roads.at(e.at(i)).lenght));
         }
     }
@@ -51,7 +52,7 @@ public:
     template<typename Func>
     void forEachEdge(Func f) const {
         for (int i = 0; i < rn.roads.size(); i++) {
-            f(ASD2::WeightedEdge<int>(rn.roads.at(i).cities.first,
+            f(ASD2::WeightedDirectedEdge<double>(rn.roads.at(i).cities.first,
                     rn.roads.at(i).cities.second, rn.roads.at(i).lenght));
         }
     }
